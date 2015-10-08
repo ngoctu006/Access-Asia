302bc7ffdbfd79fbecb74cf759d477ed5c9356c4
Tu Tran <tu.tran@sutrixmedia.com>
2015-10-08 16:36:54 +0700

    upload source

--- wp-content/themes/twentyfifteen/template-sidebar.php
+++ wp-content/themes/twentyfifteen/template-sidebar.php
@@ -79,112 +79,11 @@ get_header();
     <aside>
         <div class="nxs-sidebar-container nxs-sidebar1 nxs-widescreen nxs-widgets-editable nxs-elements-container nxs-post-27  ">
             <div class="nxs-postrows ui-sortable">
-                <div class="nxs-row nxs-not-unistyled nxs-padding-top-1-0   nxs-rowtemplate-one  " id="nxs-pagerow-prid1059454979">
-                    <div class="nxs-row-container nxs-containsimmediatehovermenu nxs-row1">
-                        <ul class="nxs-placeholder-list"> 
-                            <li class="nxs-placeholder nxs-containshovermenu1 nxs-runtime-autocellsize nxs-one-whole nxs-unistyle-sidebar nxs-unistyled nxs-not-unicontented nxs-widgettype-callout  nxs-column-1-1 nxs-column-lastinrow">
-                                <div class="ABC nxs-height100 nxs-colorzen nxs-colorzen-c32  nxs-border-radius-2-0  nxs-border-width-1-0  ">
-                                    <div class="XYZ nxs-padding-1-0">
-                                        <div class="nxs-placeholder-content-wrap nxs-crop ">  
-                                            <div id="nxs-widget-l1084107959" class="nxs-widget nxs-widget-l1084107959  nxs-callout callout720">
-                                                <a target="_blank" href="tel://123456-7890">
-                                                    <div class="nxs-align-center">
-                                                        <div class="text-wrapper">
-                                                            <h2 class="nxs-title nxs-heightiq nxs-heightiq-p1-callout-title ">Get your assets protected.<br>Call us today!</h2>
-                                                            <div class="nxs-clear nxs-filler"></div>
-                                                            <p class=" nxs-padding-bottom0">
-                                                                <span class="nxs-button nxs-button-scale-2-0  nxs-colorzen nxs-colorzen-c22  ">(123) 456-7890</span>
-                                                            </p>
-                                                            <div class="nxs-clear"></div>
-                                                        </div>
-                                                        <div class="nxs-clear"></div>
-                                                    </div>
-                                                </a>
-
-                                            </div> 
-                                        </div>
-
-                                    </div>
-
-                                </div>
-                            </li>
-                        </ul>
-                        <div class="nxs-clear"></div>
-                    </div> <!-- nxs-row-container -->
-                </div>
-                <div class="nxs-row nxs-not-unistyled  nxs-rowtemplate-one  " id="nxs-pagerow-prid1102541380">
-                    <div class="nxs-row-container nxs-containsimmediatehovermenu nxs-row1">
-                        <ul class="nxs-placeholder-list"> 
-                            <li class="nxs-placeholder nxs-containshovermenu1 nxs-runtime-autocellsize nxs-one-whole nxs-unistyle-sidebar nxs-unistyled nxs-not-unicontented nxs-widgettype-text  nxs-column-1-1 nxs-column-lastinrow">
-                                <div class="ABC">
-                                    <div class="XYZ nxs-valign-middle ">
-                                        <div class="nxs-placeholder-content-wrap nxs-crop "> 
-                                            <div id="nxs-widget-l1029420891" class="nxs-widget nxs-widget-l1029420891  nxs-text ">
-                                                <div class="nxs-applylinkvarcolor">
-                                                    <h2 class="nxs-title nxs-align-left  nxs-heightiq nxs-heightiq-p1-title  " style="height: 32px;">Our Services</h2>
-                                                </div>
-                                                <div class="nxs-clear"></div>
-
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-                            </li>
-                        </ul>
-                        <div class="nxs-clear"></div>
-                    </div>
-                </div>
-                <div class="nxs-row nxs-not-unistyled  nxs-rowtemplate-one  " id="nxs-pagerow-prid1366077984">
-                    <div class="nxs-row-container nxs-containsimmediatehovermenu nxs-row1">
-                        <ul class="nxs-placeholder-list"> 
-                            <li class="nxs-placeholder nxs-containshovermenu1 nxs-runtime-autocellsize nxs-one-whole nxs-margin-bottom-0-5  nxs-unistyle-servicebutton nxs-unistyled nxs-not-unicontented nxs-widgettype-text  nxs-column-1-1 nxs-column-lastinrow">
-                                <div class="ABC">
-                                    <div class="XYZ nxs-valign-middle " style="margin-top: 0px;">
-                                        <div class="nxs-placeholder-content-wrap nxs-crop "> 
-                                            <div id="nxs-widget-l1011550609" class="nxs-widget nxs-widget-l1011550609  nxs-text ">
-                                                <div class="top-wrapper nxs-border-width-1-0 nxs-colorzen nxs-colorzen-c12  nxs-padding-0-3 ">
-                                                    <div class="nxs-table" style=""><span class="nxs-icon-referee nxs-icon-scale-1-0 "></span><a target="_self" href="http://securitycompany.catalog.c1.us-e1.nexusthemes.com/uniformed-security/"><h4 class="nxs-title nxs-align-left  nxs-heightiq nxs-heightiq-p1-title  nxs-fontzen nxs-fontzen-3  " style="height: 25px;">Uniformed Security</h4></a>
-                                                    </div>
-                                                </div>
-                                                <div class="nxs-clear"></div>
-                                            </div> 
-                                        </div>
-                                    </div>
-                                </div>
-                            </li>
-                        </ul>
-                        <div class="nxs-clear"></div>
-                    </div> <!-- nxs-row-container -->
-                </div>
-                <div class="nxs-row nxs-not-unistyled  nxs-rowtemplate-one  " id="nxs-pagerow-prid1090465174">
-                    <div class="nxs-row-container nxs-containsimmediatehovermenu nxs-row1">
-                        <ul class="nxs-placeholder-list"> 
-                            <li class="nxs-placeholder nxs-containshovermenu1 nxs-runtime-autocellsize nxs-one-whole nxs-unistyle-sidebar nxs-unistyled nxs-not-unicontented nxs-widgettype-blog  nxs-column-1-1 nxs-column-lastinrow">
-                                <div class="ABC  nxs-colorzen nxs-colorzen-base1  nxs-border-radius-2-0  nxs-border-width-1-0  ">
-                                    <div class="XYZ nxs-padding-1-0  nxs-valign-middle ">
-                                        <div class="nxs-placeholder-content-wrap nxs-crop ">  
-                                            <div id="nxs-widget-l1310014354" class="nxs-widget nxs-widget-l1310014354  nxs-blog ">
-                                                <div class="nxs-blog-minimal nxs-blogentries nxs-paging-page-0 default">
-                                                    <h2 class="nxs-title  nxs-align-left   ">Latest News</h2>
-                                                    <div class="nxs-clear nxs-filler"></div>
-                                                    <ul>
-                                                        <li class="nxs-applylinkvarcolor nxs-default-p nxs-padding-bottom0 nxs-padding-left20">
-                                                            <span class="nxs-icon-text  font-icon"></span>
-                                                            <a href="../pellentesque-habitant-morbi-tristique-senectus/index.html"><span>Pellentesque habitant morbi tristique senectus</span></a>
-                                                        </li>
-                                                    </ul>
-                                                </div>
-                                                <p class=" nxs-padding-bottom0"></p>
-                                            </div> 
-                                        </div>
-                                    </div>
-                                </div>
-                            </li>
-                        </ul>
-                        <div class="nxs-clear"></div>
-
-                    </div> <!-- nxs-row-container -->
-                </div>
+                <?php
+                    include get_template_directory().'/templates/aside/block_contact.php';
+                    include get_template_directory().'/templates/aside/block_service.php';
+                    include get_template_directory().'/templates/aside/block_latest_new.php';
+                ?>
             </div>
         </div> 
         <!-- end nxs-sidebar-container --></aside>				
