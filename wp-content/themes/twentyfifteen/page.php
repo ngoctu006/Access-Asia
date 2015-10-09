2242839b2a1ed91d113ef6e8c72b2de3bc2941f8
ngoctu006 <tuit006@gmail.com>
2015-10-09 20:17:59 +0700

    update source

--- wp-content/themes/twentyfifteen/page.php
+++ wp-content/themes/twentyfifteen/page.php
@@ -10,18 +10,23 @@
  * @subpackage Twenty_Fifteen
  * @since Twenty Fifteen 1.0
  */
+ ?>
 
-get_header(); ?>
 
-	<div id="primary" class="content-area">
-		<main id="main" class="site-main" role="main">
-
-		<?php
+<?php
+/*
+  Template Name: Template sidebar
+ */
+get_header();
+?>
+<div id="nxs-content-container" class="nxs-containsimmediatehovermenu has-sidebar">
+    <div class="nxs-main">
+        <?php
 		// Start the loop.
 		while ( have_posts() ) : the_post();
 
 			// Include the page content template.
-			get_template_part( 'content', 'page' );
+			get_template_part( 'content', 'single' );
 
 			// If comments are open or we have at least one comment, load up the comment template.
 			if ( comments_open() || get_comments_number() ) :
@@ -31,8 +36,20 @@ get_header(); ?>
 		// End the loop.
 		endwhile;
 		?>
-
-		</main><!-- .site-main -->
-	</div><!-- .content-area -->
-
+        <!-- FRACTIONMARKER.start -->				
+        
+    </div> <!-- END nxs-main -->
+    <aside>
+        <div class="nxs-sidebar-container nxs-sidebar1 nxs-widescreen nxs-widgets-editable nxs-elements-container nxs-post-27  ">
+            <div class="nxs-postrows ui-sortable">
+                <?php
+                    include get_template_directory().'/templates/aside/block_contact.php';
+                    include get_template_directory().'/templates/aside/block_service.php';
+                    include get_template_directory().'/templates/aside/block_latest_new.php';
+                ?>
+            </div>
+        </div> 
+        <!-- end nxs-sidebar-container --></aside>				
+    <div class="nxs-clear"></div>
+</div>  
 <?php get_footer(); ?>
