8ddb5a1b21bcd94f24b6ff1dc742a2c6bedf259d
ngoctu006 <tuit006@gmail.com>
2015-10-11 09:00:14 +0700

    upload source

--- wp-content/themes/twentyfifteen/template-sidebar.php
+++ wp-content/themes/twentyfifteen/template-sidebar.php
@@ -6,29 +6,6 @@ get_header();
 ?>
 <div id="nxs-content-container" class="nxs-containsimmediatehovermenu has-sidebar">
     <div class="nxs-main">
-        <div class="nxs-subheader-container nxs-widescreen nxs-widgets-editable nxs-elements-container nxs-post-1046  ">
-            <div class="nxs-postrows ui-sortable">
-                <div class="nxs-row nxs-not-unistyled nxs-padding-top-1-0   nxs-rowtemplate-one  " id="nxs-pagerow-prid1225943937">
-                    <div class="nxs-row-container nxs-containsimmediatehovermenu nxs-row1">
-                        <ul class="nxs-placeholder-list"> 
-                            <li class="nxs-placeholder nxs-containshovermenu1 nxs-runtime-autocellsize nxs-one-whole nxs-unistyle-pagesubheaderleftaligned nxs-unistyled nxs-not-unicontented nxs-widgettype-wordpresstitle  nxs-column-1-1 nxs-column-lastinrow">
-                                <div class="ABC nxs-height100 ">
-                                    <div class="XYZ">
-                                        <div class="nxs-placeholder-content-wrap nxs-crop ">
-                                            <div id="nxs-widget-l1340073469" class="nxs-widget nxs-widget-l1340073469  nxs-wordpress-title">
-                                                <h1 class="nxs-title nxs-align-left  nxs-head-fontsize-3-0  ">News</h1>
-                                                <div class="nxs-clear"></div>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-                            </li>
-                        </ul>
-                        <div class="nxs-clear"></div>
-                    </div> <!-- nxs-row-container -->
-                </div>
-            </div>					
-        </div>
         <!-- FRACTIONMARKER.start -->				
         <div class="nxs-article-container nxs-widgets-editable nxs-elements-container nxs-post-1037 ">
             <div class="nxs-postrows ui-sortable">
@@ -42,26 +19,13 @@ get_header();
                                             <div id="nxs-widget-l1187387677" class="nxs-widget nxs-widget-l1187387677  nxs-blog ">
                                                 <div class="nxs-blog-extended nxs-blogentries nxs-paging-page-0 default">
                                                     <div class="nxs-clear nxs-filler"></div>                                                    
-                                                    <div class="nxs-blogentry nxs-relative ">
-
-                                                        <div class="sharecounters">
-                                                        </div>
-
-                                                        <div class="info-wrapper">
-                                                            <h3 class="nxs-title nxs-applylinkvarcolor  ">
-                                                                <a href="../aenean-fermentum/index.html">Aenean fermentum</a>
-                                                            </h3><div class="nxs-applylinkvarcolor"><div class="nxs-blog-meta">May 3, 2013</div><div class="nxs-blog-sharing"></div><div class="nxs-clear"></div></div>
-
-                                                        </div> 
-                                                        <div class="nxs-relative">
-                                                            <a href="../aenean-fermentum/index.html">
-                                                                <div class="nxs-image-wrapper  nxs-icon-width-1-5  ">
-                                                                    <div style="right: 0; left: 0; top: 0; bottom: 0; border-style: solid;" class="nxs-overflow nxs-border-width-0-0 "><img class=" " src="../wp-content/uploads/2015/01/article5-200x2003cdc.jpg?quality=100.3015072922390" alt="Aenean fermentum" title="Aenean fermentum"></div></div></a></div><p class="nxs-default-p nxs-padding-bottom0 "><span>Cras vel dolor magna. Nulla pellentesque, elit non gravida tempor, lacus nisl rutrum ipsum, eu convallis odio lorem non nunc. Nam dictum lorem in arcu tempus, vitae gravida metus tempor. Aenean consectetur, tortor nec imperdiet tincidunt, neque quam porta augue, et placerat nisl tellus in nulla. Vestibulum eu consequat nisl. Morbi faucibus nec nunc at ullamcorper. Suspendisse purus leo, gravida et[...]</span></p>
-                                                        <p class="nxs-align-right nxs-padding-bottom0">
-                                                            <a class="nxs-button nxs-button-scale-1-2  nxs-colorzen nxs-colorzen-c22 " href="../aenean-fermentum/index.html">Read more</a>
-                                                        </p><div class="nxs-clear nxs-padding-top20"></div>
-
-                                                    </div>
+                                                     <?php
+                                                        // Start the loop.
+                                                            while ( have_posts() ) : the_post();
+                                                                get_template_part( 'content','single');
+                                                            // End the loop.
+                                                            endwhile;
+                                                    ?>
 
                                                 </div>
                                                 <p class=" nxs-padding-bottom0"></p>
