2ccdd12280bb8539b613b03c2cbf709356978af8
anhtuanssp <anhtuan.ssp@gmail.com>
2015-10-11 11:56:42 +0700

    Update post thumbnail for contetn

--- wp-content/themes/twentyfifteen/content-single.php
+++ wp-content/themes/twentyfifteen/content-single.php
@@ -26,6 +26,29 @@
                 <div class="nxs-row nxs-not-unistyled  nxs-rowtemplate-one  " id="nxs-pagerow-1079727988">
                     <div class="nxs-row-container nxs-containsimmediatehovermenu nxs-row1">
                         <ul class="nxs-placeholder-list"> 
+                            <li class="nxs-placeholder nxs-containshovermenu1 nxs-runtime-autocellsize nxs-one-whole nxs-unistyle-main nxs-unistyled nxs-not-unicontented nxs-widgettype-image  nxs-column-1-1 nxs-column-lastinrow">
+                                <div class="ABC nxs-height100 ">
+                                    <div class="XYZ ">
+                                        <div class="nxs-placeholder-content-wrap nxs-crop "> 
+                                            <div id="nxs-widget-l1220813114" class="nxs-widget nxs-widget-l1220813114  nxs-image ">
+                                                <div class="nxs-applylinkvarcolor nxs-logo">
+                                                    <div class="nxs-relative nxs-overflow  left" style="max-width: 900px; ">
+                                                            <div style="right: 0; left: 0; top: 0; bottom: 0; border-style: solid; " 
+                                                            class="nxs-border-width-0-0  nxs-overflow ns-img-responsive">
+                                                                <?php the_post_thumbnail() ?>
+                                                            </div>
+                                                    </div>
+                                                </div>
+                                            </div> 
+                                        </div>
+                                    </div>
+                                </div>
+                            </li>
+                        </ul>
+                        <div class="nxs-clear"></div>
+                    </div>
+                    <div class="nxs-row-container nxs-containsimmediatehovermenu nxs-row1">
+                        <ul class="nxs-placeholder-list"> 
                             <li class="nxs-placeholder nxs-containshovermenu1 nxs-runtime-autocellsize nxs-one-whole nxs-unistyle-main nxs-unistyled nxs-not-unicontented nxs-widgettype-blog  nxs-column-1-1 nxs-column-lastinrow">
                                 <div class="ABC nxs-height100 nxs-linkcolorvar nxs-linkcolorvar-main2-d  ">
                                     <div class="XYZ ">
